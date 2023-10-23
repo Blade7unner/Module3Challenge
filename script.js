04200dd990f63dc81df79d387d0348c7d0d1b2f3
Felipe Benoit <benoit@Felipes-MacBook-Pro.local>
2023-10-23 16:24:52 -0400

    comments added

--- script.js
+++ script.js
@@ -32,11 +32,26 @@ console.log (getRandomElementFromArray( ['?','/','$']))
 var randomLetter=letters [ randomPosition];
 console.log (randomLetter)
 
+
+
 function generatePassword () {
   var password="Test Password!";
 /**
+ * - Version 1: generatePassword returns a random password with, only one kind of characters, and a pre-fixed length.
+ * a. Create an array with the charactres you want to used
+ * b. create an empty variable that will store the password
+ * c. loop form 0 to the legnth you choose and at every step:
+ *    - Get a random element from the array
+ *    - Append the element to the password variable
  * 
- * 
+ * - Version 2: generatePassword returns a random password with, only one kind of characters, and asks the user what lenght the user wants
+ * the password to have.
+ * a. Create an array with the charactres you want to used
+ * b. ask the user what should be the lenght (use prompt)
+ * c. create an empty variable that will store the password
+ * d. loop form 0 to the legnth you choose and at every step:
+ *    - Get a random element from the array
+ *    - Append the element to the password variable
  */
 return password;
 
