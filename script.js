8a051f5f2eba0c43b2c9cf602133326145345eae
Felipe Benoit <benoit@Felipes-MacBook-Pro.local>
2023-10-23 20:13:53 -0400

    added more comment to script.js

--- script.js
+++ script.js
@@ -1,26 +1,21 @@
-// Assignment Code
+// Assignment Code //
 var generateBtn = document.querySelector("#generate");
 
-function generatePassword() {
-  console.log("Click!")
-  var password="Input Generated Password Here!";
 
-  return password;
-  
-  }
-// character set (in this case, lowercase letters)
+
+// character set (in this case, lowercase letters) //
 var lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
 
-// Function to generate a random character from the lowercase character set
+// Function to generate a random character from the lowercase character set //
 function getRandomLowercaseChar() {
   var randomIndex = Math.floor(Math.random() * lowercaseChars.length);
   return lowercaseChars[randomIndex];
 }
 
-// Function to generate a random password with a fixed length and only lowercase characters
+// Function to generate a random password with a fixed length and only lowercase characters //
 function generatePassword() {
   var password = "";
-  var length = 10; // change this to desired password length
+  var length = 10; // change this to desired password length //
 
   for (var i = 0; i < length; i++) {
     var randomChar = getRandomLowercaseChar();
@@ -32,7 +27,7 @@ function generatePassword() {
 
 
 
-// Write password to the #password input
+// Write password to the #password input //
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -41,5 +36,5 @@ function writePassword() {
 
 }
 
-// Add event listener to generate button
+// Add event listener to generate button //
 generateBtn.addEventListener("click", writePassword);
